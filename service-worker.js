c13c028632ae54af5aa1c38d234c19a5edc3206d
piupin <piupin0000@gmail.com>
2025-08-15 00:08:38 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,71 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('star-cache-v1').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icons/icon-192x192.png',
-        './icons/icon-512x512.png'
-      ]);
+const CACHE_NAME = "student-stars-cache-v1";
+const DATA_CACHE_NAME = "student-stars-data-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+// Install service worker
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("[ServiceWorker] Pre-caching offline page");
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+// Activate service worker
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch handler
+self.addEventListener("fetch", (evt) => {
+  const sheetBaseURL = "https://docs.google.com/spreadsheets";
+
+  if (evt.request.url.includes(sheetBaseURL)) {
+    // Handle Google Sheets data
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(evt.request)
+          .then((response) => {
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(() => {
+            return cache.match(evt.request);
+          });
+      })
+    );
+    return;
+  }
+
+  // Default fetch for static files
+  evt.respondWith(
+    caches.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
     })
   );
 });
